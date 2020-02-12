c68013a8cae5aa43cb2c10303c6c540a39411997
Alan Bernstein <alan.bernstein@outlook.com>
2020-02-13 00:15:57 +0100

    first day done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LogRocket from "logrocket";
+import * as Sentry from "@sentry/browser";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    if (process.env.NODE_ENV === 'production') {
+        LogRocket.init('s3mkoe/saim-mobilephysio');
+        LogRocket.getSessionURL(sessionURL => {
+            Sentry.configureScope(scope => {
+                scope.setExtra("sessionURL", sessionURL);
+            });
+        });
+    }
+
+    return (
+        <div className="App">
+            <nav className={'nav'}>
+                <div className={'navWrapper'}>
+                    <a href="#home">Home</a>
+                    <a href="#intro">Intro</a>
+                    <a href="#services">Leistungen</a>
+                    <a href="#prices">Preise</a>
+                    <a href="#contact">Kontakt</a>
+                </div>
+            </nav>
+            <header className={'header'} id={'home'}>
+                <div className={'headerContainer'}>
+                    <div className={'headerWrapper'}>
+                        <div className={'headerContent'}>
+                            <h1><span>Saim</span>Mobile Physiotherapie</h1>
+                            <hr/>
+                            <div className={'headerParagraph'}>
+                                <p>{new Date().getFullYear() - 1999} Jahre Erfahrung</p>
+                                <p>Faranak Nokhbehzaeem</p>
+                            </div>
+                        </div>
+                        <picture className={'picture'}>
+                            <source srcSet={require('./img/faranak.webp')} type={'image/webp'}/>
+                            <source srcSet={require('./img/faranak.png')} type={'image/png'}/>
+                            <img src={require('./img/faranak.png')} alt=""/>
+                        </picture>
+                    </div>
+                </div>
+            </header>
+            <section className={'intro'} id={'intro'}>
+
+            </section>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/browser';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (process.env.NODE_ENV === 'production') {
+    Sentry.init({dsn: "https://524feab13c5a4ed7897bb6af2e3fa0d2@sentry.io/2279347"});
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
