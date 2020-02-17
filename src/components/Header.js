5e12a2a6e77a39784aae92a6dce65e266fc90f61
Alan Bernstein <alan.bernstein@outlook.com>
2020-02-17 19:43:54 +0100

    ready for push

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styles from '../styles/components/Header.module.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPhone} from "@fortawesome/free-solid-svg-icons/faPhone";
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons/faEnvelope";
 
 const Header = () => {
     return (
@@ -8,20 +11,32 @@ const Header = () => {
                 <div className={styles.left}>
                     <div className={styles.svg}/>
                     <div className={styles.container}>
-                        <nav></nav>
+                        <nav className={styles.nav}>
+                            <img src={require('../img/logo.svg')} alt="logo" width={50} height={50}/>
+                            <a href="">Home</a>
+                            <a href="">Über mich</a>
+                            <a href="">Leistungen</a>
+                            <a href="">Preise</a>
+                            <a href="">Kontakt</a>
+                        </nav>
                         <h1 children={"Saim Mobile Physiotherapie"}
                             className={styles.h1}
                         />
-                        <h2 children={"Privatpatienten"}
+                        <h2 children={"Privat"}
                             className={styles.h2}
                         />
-                        <div>
+                        <div className={styles.subHeading}>
                             <div>
-
+                                <p>{new Date().getFullYear() - 1999} Jahre Berufserfahrung</p>
                             </div>
                             <div>
-
+                                <p>Staatlich anerkannte Manualtherapeutin, Physiotherapeutin, Ödemtherapeutin und
+                                    Personal Trainerin</p>
                             </div>
+                        </div>
+                        <div className={styles.icons}>
+                            <a><FontAwesomeIcon icon={faPhone} size={'2x'}/></a>
+                            <a><FontAwesomeIcon icon={faEnvelope} size={'2x'}/></a>
                         </div>
                     </div>
                 </div>
