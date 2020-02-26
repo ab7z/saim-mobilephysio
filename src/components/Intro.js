9cfb18f54b0984d72ff5add1c0e2517e5cec3b4b
Alan Bernstein <alan.bernstein@outlook.com>
2020-02-26 19:25:50 +0100

    caching

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -15,33 +15,33 @@ const Intro = () => {
                             Ödem–Thera­peutin, Kos­metik­erin und Friseurin. Die Ausbildung zur
                             Physiotherapeutin habe ich im Iran (mit 6 Jahre Berufserfahrung) und anschließend
                             noch einmal in Deutschland abgeschlossen und mit folgenden Zusatz-Fortbildungen abgerundet:
-                            <ul>
-                                <li className={styles.li}>Manu­elle Thera­pie</li>
-                                <li className={styles.li}>Manuelle Lym­phdrainage</li>
-                                <li className={styles.li}>Kranken­gym­nastik</li>
-                                <li className={styles.li}>Vibra­fit­training</li>
-                                <li className={styles.li}>Kine­sio­log­ische Tape­tech­niken</li>
-                            </ul>
                         </p>
+                        <ul>
+                            <li className={styles.li}>Manu­elle Thera­pie</li>
+                            <li className={styles.li}>Manuelle Lym­phdrainage</li>
+                            <li className={styles.li}>Kranken­gym­nastik</li>
+                            <li className={styles.li}>Vibra­fit­training</li>
+                            <li className={styles.li}>Kine­sio­log­ische Tape­tech­niken</li>
+                        </ul>
                     </div>
                     <div className={styles.desc2}>
                         <p className={styles.p}>Seitdem arbeite ich ununterbrochen in Festanstellung und biete meine
                             Dienstleistung nebenberuflich auch selbstständig an. Meine Fach­ken­ntnis­se im
                             Krankenhausbereich, Teilnehme an Operationen gliedern sich durch:
-                            <ul>
-                                <li className={styles.li}>Helios Klinikum Siegburg in den Abteilungen Chirurgie, Innere
-                                    Medizin und
-                                    Kardiologie
-                                </li>
-                                <li className={styles.li}>Sankt Josef Hospital in Bonn in den Abteilungen Orthopädie,
-                                    Chirurgie und
-                                    Rücken­schmerz
-                                </li>
-                                <li className={styles.li}>und Elisabeth Krankenhaus in Bonn in den Abteilungen Geriatrie
+                        </p>
+                        <ul>
+                            <li className={styles.li}>Helios Klinikum Siegburg in den Abteilungen Chirurgie, Innere
+                                Medizin und
+                                Kardiologie
+                            </li>
+                            <li className={styles.li}>Sankt Josef Hospital in Bonn in den Abteilungen Orthopädie,
+                                Chirurgie und
+                                Rücken­schmerz
+                            </li>
+                            <li className={styles.li}>und Elisabeth Krankenhaus in Bonn in den Abteilungen Geriatrie
                                     und Gynäkologie.
                                 </li>
                             </ul>
-                        </p>
                     </div>
                 </div>
             </section>
