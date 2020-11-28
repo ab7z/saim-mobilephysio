e8cae9422c1a6b94d4fdca5067c15926e41e314b
Alan Bernstein <alan.bernstein@outlook.com>
2020-11-28 17:36:33 +0100

    update prices

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -7,9 +7,9 @@ const Prices = () => {
         <React.Fragment>
             <section className={styles.prices} id={'prices'}>
                 <div className={styles.wrapper}>
-                    <h2 className={styles.heading}>Preise &mdash; 120€ pro 60 Minuten</h2>
+                    <h2 className={ styles.heading }>Preise &mdash; 130€ pro 60 Minuten</h2>
                     <div>
-                        <p className={styles.p}>Ich bin eine Privat­praxis und habe keinen Vertrag mit einer
+                        <p className={ styles.p }>Ich bin eine Privat­praxis und habe keinen Vertrag mit einer
                             gesetz­lichen Kranken­kas­se.
                             Ich stelle meine Rech­nung direkt an Sie als Patient und Sie reichen diese an Ihre private
                             Kran­ken­ver­sich­erung (PKV) zur Er­stat­tung weiter. Ihre Ver­sicher­ung er­stat­tet
@@ -23,14 +23,14 @@ const Prices = () => {
                             <div className={[styles.rect, styles.effect5].join(' ')}>
                                 <p>Im Normal­fall (Be­hand­lung aufgrund einer Krank­heit) legen Sie mir ein Rezept von
                                     Ihrem Arzt vor. Der Preis pro The­rapie­sit­zung (60 Minuten) be­trägt in diesem
-                                    Fall <b>120€</b>. Mehrwertsteuer fällt nicht an.</p>
+                                    Fall <b>130€</b>. Mehrwertsteuer fällt nicht an.</p>
                             </div>
                             <div className={[styles.rect, styles.effect5].join(' ')}>
                                 <p>Wenn Sie keine Beschwerden haben, sondern präventiv (ohne ärztliche Verordnung) von
-                                    mir behandelt werden möchten, liegt der Preis bei <b>140€</b> inkl. Mehrwertsteuer.</p>
+                                    mir behandelt werden möchten, liegt der Preis bei <b>150€</b> inkl. Mehrwertsteuer.
+                                </p>
                             </div>
                         </div>
-                        <p>Zu­sätz­liche Kosten für An- und Ab­fahrt be­rech­ne ich nicht.</p>
                     </div>
                     <div>
                         <p><b>Füllen Sie vor Ihrer ersten Behandlung bitte das <a href={PDF}
