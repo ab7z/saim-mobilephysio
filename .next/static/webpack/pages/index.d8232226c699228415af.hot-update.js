3a030cb42c9c4bd38925a6b60a6502d1f23a89e4
Alan Bernstein <alan.bernstein@deepup.de>
2021-07-22 00:25:09 +0200

    .

---
+++ .next/static/webpack/pages/index.d8232226c699228415af.hot-update.js
@@ -0,0 +1,22 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./public/service.webp":
+/*!*****************************!*\
+  !*** ./public/service.webp ***!
+  \*****************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/image/public/service.fc2128d2283e395e4faed1decf342f91.webp\",\"height\":2624,\"width\":3936,\"blurDataURL\":\"data:image/webp;base64,UklGRngAAABXRUJQVlA4WAoAAAAQAAAABwAABAAAQUxQSBIAAAABD9D/iAgIBIgN2qUZi+h/PAZWUDggQAAAANABAJ0BKggABQACQDglAE6AI+1hkaUAAP7wfpeag5XF7Mr1JbflAcw3f+0GCeiI7UYUXAAnBkWy6rZkZnrVgAA=\"});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vcHVibGljL3NlcnZpY2Uud2VicD82ZGRiIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSwrREFBZ0IsQ0FBQywySUFBMkkscUxBQXFMIiwiZmlsZSI6Ii4vcHVibGljL3NlcnZpY2Uud2VicC5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0ICB7XCJzcmNcIjpcIi9fbmV4dC9zdGF0aWMvaW1hZ2UvcHVibGljL3NlcnZpY2UuZmMyMTI4ZDIyODNlMzk1ZTRmYWVkMWRlY2YzNDJmOTEud2VicFwiLFwiaGVpZ2h0XCI6MjYyNCxcIndpZHRoXCI6MzkzNixcImJsdXJEYXRhVVJMXCI6XCJkYXRhOmltYWdlL3dlYnA7YmFzZTY0LFVrbEdSbmdBQUFCWFJVSlFWbEE0V0FvQUFBQVFBQUFBQndBQUJBQUFRVXhRU0JJQUFBQUJEOUQvaUFnSUJJZ04ycVVaaStoL1BBWldVRGdnUUFBQUFOQUJBSjBCS2dnQUJRQUNRRGdsQUU2QUkrMWhrYVVBQVA3d2ZwZWFnNVhGN01yMUpiZmxBY3czZiswR0NlaUk3VVlVWEFBbkJrV3k2clprWm5yVmdBQT1cIn07Il0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./public/service.webp\n");
+
+/***/ })
+
+});
